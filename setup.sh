5553a29d1cfbabe40d9335aa5ead62152344e641
Thorsten Staerk <dev@staerk.de>
2013-03-13 21:25:43 +0100

    show the problems to the user

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ die() { echo "$@" 1>&2 ;exit 1; }
 cd $(dirname $0)
 mkdir -p build 2>/dev/null || die "Cannot create a directory named \"build\". Please fix your permissions."
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` .. 2>/dev/null || die "Cannot call cmake. Please install cmake, gcc and libkde4-devel."
-make -j8 2>/dev/null || die "Cannot build. Make sure the software 'make' is installed."
-sudo make install 2>/dev/null || die "Cannot install. Make sure you are root."
+cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` .. || die "cmake did not succeed. Please install cmake, gcc and libkde4-devel."
+make -j8 || die "Cannot build. Make sure the software 'make' is installed."
+sudo make install || die "Cannot install. Make sure you are root."
 
 # re-discover .desktop files
 kbuildsycoca4 2>/dev/null || die "Cannot call kbuildsycoca4. Make sure KDE 4 is installed."
